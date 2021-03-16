856a3735e0d1a0da841ec78765fdbb0e9d08451e
Anhlaidh <chinahantianzhao@foxmail.com>
2021-03-17 00:51:10 +0800

    add eureka

---
+++ prs-eureka/src/main/java/edu/prs/eureka/EurekaApplication.java
@@ -0,0 +1,19 @@
+package edu.prs.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Description:
+ * @author: Anhlaidh
+ * @date: 2021-03-17 00:37
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
