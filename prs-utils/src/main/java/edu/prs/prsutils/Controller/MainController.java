8f937f1d481c3b7c01ea61d8054dde65ae002b22
Anhlaidh <chinahantianzhao@foxmail.com>
2021-03-17 12:15:54 +0800

    add redis

--- prs-utils/src/main/java/edu/prs/prsutils/Controller/MainController.java
+++ prs-utils/src/main/java/edu/prs/prsutils/Controller/MainController.java
@@ -1,10 +1,16 @@
 package edu.prs.prsutils.Controller;
 
+import com.alibaba.fastjson.JSON;
 import edu.prs.prsutils.Mapper.StationInfoMapper;
 import edu.prs.prsutils.Model.StationInfo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @Description:
@@ -15,9 +21,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
     @Autowired
     StationInfoMapper mapper;
+    @Autowired
+    StringRedisTemplate template;
+
     @RequestMapping("test")
-    public StationInfo test() {
-        System.out.println(mapper.selectByPrimaryKey(1).getName());
-        return mapper.selectByPrimaryKey(15);
+    public String test() {
+        List<StationInfo> list = new ArrayList<>();
+        StationInfo info = mapper.selectByPrimaryKey(1);
+        System.out.println(info);
+        list.add(info);
+        String s = JSON.toJSONString(info);
+        return s;
+    }
+
+    @RequestMapping("testRedis")
+    public String RedisTest() {
+        Long id = template.opsForValue().increment("id");
+        return id+"";
     }
 }
--- prs-utils/src/test/java/edu/prs/prsutils/PrsUtilsApplicationTests.java
+++ prs-utils/src/test/java/edu/prs/prsutils/PrsUtilsApplicationTests.java
@@ -3,6 +3,9 @@ package edu.prs.prsutils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisAccessor;
+import org.springframework.data.redis.core.RedisCommand;
+import org.springframework.data.redis.core.RedisTemplate;
 
 import javax.sql.DataSource;
 import java.sql.SQLException;
@@ -11,11 +14,17 @@ import java.sql.SQLException;
 class PrsUtilsApplicationTests {
     @Autowired
     DataSource dataSource;
+    @Autowired
+    RedisTemplate<String ,Object> template;
 
     @Test
     void contextLoads() throws SQLException {
         System.out.println(dataSource);
         System.out.println(dataSource.getConnection());
+        template.opsForValue().set("test",1);
+        Object test = template.opsForValue().get("test");
+        System.out.println(test);
+
     }
 
 }
